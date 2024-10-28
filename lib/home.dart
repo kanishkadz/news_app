302fd4cf1dd90db327cd08b60eec9b9d6cb934d8
Kanishka Anand <kanishkaadz@gmail.com>
2024-10-28 21:49:28 +0530

    Update home.dart

--- lib/home.dart
+++ lib/home.dart
@@ -1,1 +1,66 @@
-importM
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("NEWS PULSE"),
+        centerTitle: true,,
+      ),
+      body: Column(
+        children: [
+      //Search Wala Container
+
+      padding: EdgeInsets.symmetric(horizontal: 8),
+      margin: EdgeInsets.symmetric(horizontal: 24, vertical: 20),
+      decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(24)),
+      child: Row(
+        children: [
+          GestureDetector(
+            onTap: () {
+              if ((searchController.text).replaceAll(" ", "") ==
+                  "") {
+                print("Blank search");
+              } else {
+                // Navigator.push(context, MaterialPageRoute(builder: (context) => Search(searchController.text)));
+              }
+            },
+            child: Container(
+              child: Icon(
+                Icons.search,
+                color: Colors.blueAccent,
+              ),
+              margin: EdgeInsets.fromLTRB(3, 0, 7, 0),
+            ),
+          ),
+          Expanded(
+            child: TextField(
+              controller: searchController,
+              textInputAction: TextInputAction.search,
+              onSubmitted: (value){
+                print(value);
+              },
+              decoration: InputDecoration(
+                  border: InputBorder.none,
+                  hintText: "Search Health"),
+            ),
+          )
+        ],
+      ),
+    ),
+
+    ],
+      ),
+    );
+  }
+}
