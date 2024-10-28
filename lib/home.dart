f6a4ad3c0b7561eff4a88cab078d29fcab0a9d2c
Kanishka Anand <kanishkaadz@gmail.com>
2024-10-28 21:51:57 +0530

    Update home.dart

--- lib/home.dart
+++ lib/home.dart
@@ -8,7 +8,8 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  TextEditingController searchController = new TextEditingController();
+  TextEditingController searchController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,49 +19,50 @@ class _HomeState extends State<Home> {
       ),
       body: Column(
         children: [
-      //Search Wala Container
-
-      padding: EdgeInsets.symmetric(horizontal: 8),
-      margin: EdgeInsets.symmetric(horizontal: 24, vertical: 20),
-      decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(24)),
-      child: Row(
-        children: [
-          GestureDetector(
-            onTap: () {
-              if ((searchController.text).replaceAll(" ", "") ==
-                  "") {
-                print("Blank search");
-              } else {
-                // Navigator.push(context, MaterialPageRoute(builder: (context) => Search(searchController.text)));
-              }
-            },
-            child: Container(
-              child: Icon(
-                Icons.search,
-                color: Colors.blueAccent,
-              ),
-              margin: EdgeInsets.fromLTRB(3, 0, 7, 0),
+          // Search Container
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 8),
+            margin: EdgeInsets.symmetric(horizontal: 24, vertical: 20),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(24),
+            ),
+            child: Row(
+              children: [
+                GestureDetector(
+                  onTap: () {
+                    if ((searchController.text).replaceAll(" ", "") == "") {
+                      print("Blank search");
+                    } else {
+                      // Uncomment the following line to navigate to the search page
+                      // Navigator.push(context, MaterialPageRoute(builder: (context) => Search(searchController.text)));
+                    }
+                  },
+                  child: Container(
+                    child: Icon(
+                      Icons.search,
+                      color: Colors.blueAccent,
+                    ),
+                    margin: EdgeInsets.fromLTRB(3, 0, 7, 0),
+                  ),
+                ),
+                Expanded(
+                  child: TextField(
+                    controller: searchController,
+                    textInputAction: TextInputAction.search,
+                    onSubmitted: (value) {
+                      print(value);
+                    },
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      hintText: "Search Health",
+                    ),
+                  ),
+                ),
+              ],
             ),
           ),
-          Expanded(
-            child: TextField(
-              controller: searchController,
-              textInputAction: TextInputAction.search,
-              onSubmitted: (value){
-                print(value);
-              },
-              decoration: InputDecoration(
-                  border: InputBorder.none,
-                  hintText: "Search Health"),
-            ),
-          )
         ],
-      ),
-    ),
-
-    ],
       ),
     );
   }
