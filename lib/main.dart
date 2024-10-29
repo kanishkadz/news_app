70ed45ac8c564e0aa6b7cbd2616712e7b33cffd0
Kanishka Anand <kanishkaadz@gmail.com>
2024-10-29 21:49:28 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -5,29 +5,8 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatefulWidget {
-  const MyApp({super.key});
-
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  bool showingSplash = true;
-  LoadHome() {
-    Future.delayed(Duration(seconds: 3), () {
-      setState(() {
-        showingSplash = false;
-      });
-    });
-  }
-
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    LoadHome();
-  }
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -37,7 +16,7 @@ class _MyAppState extends State<MyApp> {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: showingSplash ? SplashScreen() : HomeScreen(),
+      home: const HomeScreen(),
     );
   }
 }
