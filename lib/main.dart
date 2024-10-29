444dcdd7508dd56696fa040eab410d851d4bd55b
Kanishka Anand <kanishkaadz@gmail.com>
2024-10-29 21:29:36 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:news_snack/view/home.dart';
-import 'package:news_snack/view/spalsh.dart';
+import 'package:news_app/view/home.dart';
+import 'package:news_app/view/splash.dart';
 
 void main() {
   runApp(const MyApp());
@@ -34,7 +34,7 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'News Snack',
+      title: 'News Pulse',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
